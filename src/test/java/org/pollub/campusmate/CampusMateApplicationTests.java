ad276b24f0e1386c904c58bd267b742e9095bb1a
Valentine0604 <ewecia.s@gmail.com>
2024-10-13 11:20:32 +0200

    init

---
+++ src/test/java/org/pollub/campusmate/CampusMateApplicationTests.java
@@ -0,0 +1,13 @@
+package org.pollub.campusmate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CampusMateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
