f17f1a06d54c9951cd35971689b204b12a425494
Valentine0604 <ewecia.s@gmail.com>
2024-10-21 17:53:36 +0200

    init

---
+++ src/main/java/org/pollub/campusmate/CampusMateApplication.java
@@ -0,0 +1,13 @@
+package org.pollub.campusmate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CampusMateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CampusMateApplication.class, args);
+    }
+
+}
